ef868d391f90f57fd58b5b325b0a424c9dff94dd
213649163 <reemabedalrhman17@gmail.com>
2025-11-11 13:21:13 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,8 +1,6 @@
 def approximate_pi(n_terms):
-     total= 0
-     for i in range(n_terms):
-         total+=((-1)**i/(2*i+1))
-     pi_value = 4* total
-     return pi_value      
-     return approximate_pi
-approximate_pi(1000)     
+    apx_pi = 0 
+    for i in range(n_terms):
+        apx_pi+=4*((-1)**i/(2*i+1))
+    return apx_pi 
+     
